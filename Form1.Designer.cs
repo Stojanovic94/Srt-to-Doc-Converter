a45c1fe816b17725193fe687d9a52c7982e7c34b
Stojanovic94 <153657652+Stojanovic94@users.noreply.github.com>
2024-03-04 23:08:50 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace WinFormsApp9
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            richTextBox1 = new System.Windows.Forms.RichTextBox();
+            button2 = new System.Windows.Forms.Button();
+            button3 = new System.Windows.Forms.Button();
+            openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            button4 = new System.Windows.Forms.Button();
+            button1 = new System.Windows.Forms.Button();
+            SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new System.Drawing.Point(38, 11);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new System.Drawing.Size(512, 288);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "";
+            richTextBox1.TextChanged += richTextBox1_TextChanged;
+            // 
+            // button2
+            // 
+            button2.Location = new System.Drawing.Point(570, 11);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(66, 34);
+            button2.TabIndex = 2;
+            button2.Text = "Open";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new System.Drawing.Point(570, 265);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(66, 34);
+            button3.TabIndex = 3;
+            button3.Text = "Clear All";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.FileOk += openFileDialog1_FileOk;
+            // 
+            // button4
+            // 
+            button4.Location = new System.Drawing.Point(570, 119);
+            button4.Name = "button4";
+            button4.Size = new System.Drawing.Size(66, 34);
+            button4.TabIndex = 4;
+            button4.Text = "Save";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(570, 66);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(66, 34);
+            button1.TabIndex = 5;
+            button1.Text = "Convert";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(677, 359);
+            Controls.Add(button1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(richTextBox1);
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Srt to Doc Converter 4.3.2024 ";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Permissions;
+
+
+namespace WinFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //Otvori open dijalog i trazi txt fajlove
+               openFileDialog1.DefaultExt = "srt";
+               openFileDialog1.Filter =
+               "Subtitle text (*.srt)|*.srt|All files (*.*)|*.*";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+                System.IO.StreamReader sr = new System.IO.StreamReader(openFileDialog1.FileName);
+                richTextBox1.Text = sr.ReadToEnd();
+                sr.Close();
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Richtext 01_01: Create the Save File dialog Instance
+            SaveFileDialog saveDlg = new SaveFileDialog();
+            string filename = "";
+
+            //Richtext 01_02: Set the Filters for the save dialog.
+            saveDlg.Filter = "Microsoft Word (*.doc)|*.doc|All files (*.*)|*.*"; //Don't include space when when typing *.ext. Because space is treated as extension
+            saveDlg.DefaultExt = "*.doc";
+            saveDlg.FilterIndex = 1;
+            saveDlg.Title = "Save the contents";
+
+            //Richtext 01_03: Show the save file dialog
+            DialogResult retval = saveDlg.ShowDialog();
+            if (retval == DialogResult.OK)
+                filename = saveDlg.FileName;
+            else
+                return;
+
+            //Richtext 01_04: Set the correct stream type (Rich text or Plain text?)
+            RichTextBoxStreamType stream_type;
+            if (saveDlg.FilterIndex == 2)
+                stream_type = RichTextBoxStreamType.PlainText;
+            else
+                stream_type = RichTextBoxStreamType.RichText;
+
+            //Richtext 01_05: Now its time to save the content
+            richTextBox1.SaveFile(filename, stream_type);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            //KORAK 1
+            var text = "";//Holds the text of current line being looped.
+            var startindex = 0;//The position where selection starts.
+            var endindex = 2000;//The length of selection.
+
+            for (int i = 0; i < richTextBox1.Lines.Length; i++)//Loops through each line of text in RichTextBox
+            {
+                text = richTextBox1.Lines[i];//Stores current line of text.
+                if (text.Contains("-->") == true)//Checks if the line contains MOVE STORAGE.
+                    
+                    {
+                    startindex = richTextBox1.GetFirstCharIndexFromLine(i);//If match is found the index of first char of that line is stored in startindex.
+                    endindex = text.Length;//Gets the length of line till semicolon and stores it in endindex.
+                    richTextBox1.Select(startindex, endindex);//Selects the text.
+                    richTextBox1.Text = richTextBox1.Text.Replace(richTextBox1.SelectedText, string.Empty);//Replaces the text with empty string.
+                }
+            }
+
+
+            //Korak 2
+            richTextBox1.Text = richTextBox1.Text.Remove(0, richTextBox1.Lines[0].Length);
+            //Brise prazna mesta na richtextbox
+            richTextBox1.Lines = richTextBox1.Lines.Where(line => line.Trim() != string.Empty).ToArray();
+
+            //KORAK 3
+            string[] lines = richTextBox1.Text.Split('\n');
+            StringBuilder updatedText = new StringBuilder();
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (i % 2 == 0) // Check if it's an even-numbered line
+                {
+                    updatedText.AppendLine(lines[i]);
+                }
+            }
+
+            richTextBox1.Text = updatedText.ToString();
+
+            //Korak 4 preuredi text
+            string mergedText = string.Join(" ", richTextBox1.Lines);
+            richTextBox1.Text = mergedText;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
